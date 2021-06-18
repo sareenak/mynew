87b973a4402630ef56dcc8304aa12660172ad387
sareenak <43492259+sareenak@users.noreply.github.com>
2021-06-18 11:40:12 +0400

    http connection

---
+++ server.js
@@ -0,0 +1,15 @@
+var express=require("express")
+var app=express()
+var http=require("http").createServer(app)
+var io=require("socket.io")(http)
+
+app.get("/",(req,res)=>{
+	res.sendFile(__dirname+"/public/noticeboard.html");
+})
+app.get("/admin",(req,res)=>{
+	res.sendFile(__dirname+"/public/admin.html");
+})
+
+http.listen(3000,()=>{
+	console.log("connected to serve");
+})
